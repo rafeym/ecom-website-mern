0ff1aa8c0c5def7956053c7a76bb2e00c7eae80f
rafeym <mohammadrafey0@gmail.com>
2019-11-15 01:45:45 -0500

    UPDATE: client side data fetching from the server removed

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import axios from 'axios'
+import ProductList from '../components/Index/ProductList'
 
 function Home({products}) {
-  console.log(products)
-
-  // React.useEffect(() => {
-  //   getProducts()
-  // }, [])
-
-  // fetching data on the client
-  // const getProducts = async () => {
-  //   const url = 'http://localhost:3000/api/product'
-  //   const response = await axios.get(url)
-  //   console.log(response.data)
-  // }
-
-  return <>home</>
+  return <ProductList products={products}/>
 }
 
 // Fetching data from the server even before the compnent mounts
 // Getting inital data which is being added to the props paramter of component itself
 Home.getInitialProps = async () => {
   // 1. Fetch data on the server
-  const url = 'http://localhost:3000/api/product'
+  const url = 'http://localhost:3000/api/products'
   const response = await axios.get(url)
   // 2. Return that response data as an object
   return {products: response.data}
