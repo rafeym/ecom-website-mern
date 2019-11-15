54b2c1174c5b1cb715c67663846a41f5ae7d899c
rafeym <mohammadrafey0@gmail.com>
2019-11-15 16:52:52 -0500

    Client side product creation complete.

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,112 @@
-function CreateProduct() {
-  return <>create</>;
+import React from 'react'
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon
+} from 'semantic-ui-react'
+
+const INITIAL_PRODUCT = {
+  name: '',
+  price: '',
+  media: '',
+  description: ''
 }
 
-export default CreateProduct;
+function CreateProduct() {
+  const [product, setProduct] = React.useState(INITIAL_PRODUCT)
+  const [mediaPreview, setMediaPreview] = React.useState('')
+  const [success, setSuccess] = React.useState(false)
+
+  const handleChange = event => {
+    const { name, value, files } = event.target
+
+    // To handle media we must use files from event.target to store the actual file not the path
+    if (name === 'media') {
+      setProduct(prevState => ({ ...prevState, media: files[0] }))
+      setMediaPreview(window.URL.createObjectURL(files[0]))
+    } else {
+      // Use updater pattern to save previous state and append state changes to it
+      setProduct(prevState => ({ ...prevState, [name]: value }))
+    }
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(product)
+    
+    // Turn state values back to default values after submit
+    setProduct(INITIAL_PRODUCT)
+    // Set success message state to true
+    setSuccess(true)
+  }
+
+  return (
+    <>
+      <Header as='h2' block>
+        <Icon name='add' color='orange' />
+        Create New Product
+      </Header>
+      <Form success={success} onSubmit={handleSubmit}>
+      <Message
+        success
+        icon="check"
+        header="Sucess!"
+        content="Your product has been posted"
+      />
+        <Form.Group widths='equal'>
+          <Form.Field
+            control={Input}
+            name='name'
+            label='Name'
+            placeholder='Name'
+            value={product.name}
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name='price'
+            label='Price'
+            placeholder='Price'
+            min='0.00'
+            step='0.01'
+            type='number'
+            value={product.price}
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name='media'
+            type='file'
+            label='Media'
+            accept='image/*'
+            content='Select Image'
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Image src={mediaPreview} rounded centered size='small' />
+        <Form.Field
+          control={TextArea}
+          name='description'
+          label='Description'
+          placeholder='Description'
+          value={product.description}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Button}
+          color='blue'
+          icon='pencil alternate'
+          content='Submit'
+          type='submit'
+        />
+      </Form>
+    </>
+  )
+}
+
+export default CreateProduct
