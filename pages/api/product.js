85bbaa42390c5a882591ac1fb7404c263232199b
rafeym <mohammadrafey0@gmail.com>
2019-11-15 02:42:37 -0500

    Product page updated. Product apo endpoint now handles product delete requests

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,8 +1,28 @@
 import Product from '../../models/Product'
 
 export default async (req, res) => {
-  const { _id } = req.query
-  const product = await Product.findOne({ _id: _id })
+  switch (req.method){
+    case "GET":
+      await handleGetRequest(req,res)
+      break
+    case "DELETE":
+      await handleDeleteRequest(req,res)  
+      break
+    default:
+      // Indicates error on requst on client side
+      res.status(405).send(`Method ${req.method} not allowed`)
+      break
+  }
+}
 
+const handleGetRequest = async (req,res) => {
+  const { _id } = req.query
+  const product = await Product.findOne({_id})
   res.status(200).json(product)
+}
+
+const handleDeleteRequest = async (req,res) => {
+  const {_id} = req.query
+  await Product.findOneAndDelete({_id})
+  res.status(204).json({})
 }
