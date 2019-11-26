478858937fbcce063f24a22b135e02a63f2a069c
rafeym <mohammadrafey0@gmail.com>
2019-11-25 22:07:26 -0500

    Client side validation for name, email and password

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -11,24 +11,32 @@ connectDb()
 export default async (req,res) => {
     const { name, email, password } = req.body
     try {
-        // 1) Check to see if user already exists in DB
+        // 1) Validate the name/email/password values
+        if(!name.isLength(name,{ min: 2, max: 10 })){
+            res.status(422).send("Name must be 2-10 characters long")
+        } else if (!isLength(password, {min: 6})){
+            res.status(422).send("Password must be atleast 6 characters long")
+        } else if (!isEmail(email)){
+            res.status(422).send("Email must be valid")
+        }
+        // 2) Check to see if user already exists in DB
         const user = await User.findOne({email})
         if(user){
             return res.status(422).send(`User already exists with email ${email}`)
         }
-        // 2) --if not, hash their password
+        // 3) --if not, hash their password
         const hash = await bcrypt.hash(password, 10)
-        // 3) Create user
+        // 4) Create user
         const newUser = await new User({
             name,
             email,
             password: hash
         }).save()
         console.log({newUser})
-        // 4) Create token for the new user
+        // 5) Create token for the new user
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
             expiresIn: '7d' })
-        // 5) Send back token
+        // 6) Send back token
         res.status(201).json(token)
     } catch (error) {
         console.error(error)
