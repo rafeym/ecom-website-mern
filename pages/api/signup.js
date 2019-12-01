76f2ae35f5d73c30b004113eb1a6bcc3ce89bee1
rafeym <mohammadrafey0@gmail.com>
2019-12-01 04:15:40 -0500

    Signup api endpoint refactored and user creation error fixed

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,6 +1,5 @@
 import connectDb from '../../utils/connectDb'
 import User from '../../models/User'
-
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import isEmail from 'validator/lib/isEmail'
@@ -8,39 +7,40 @@ import isLength from 'validator/lib/isLength'
 
 connectDb()
 
-export default async (req,res) => {
-    const { name, email, password } = req.body
-    try {
 
-        // 1) Validate the name/email/password values
-        if(!name.isLength(name,{ min: 3, max: 20 })){
-            res.status(422).send("Name must be 2-10 characters long")
+export default async (req,res) => {
+    const {name, email, password} = req.body
+    try {
+        // 1. Validate name, email and password
+        if(!isLength(name, {min: 3, max: 256})){
+            return res.status(422).send("Name must be 3 or more characters long")
         } else if (!isLength(password, {min: 8})){
-            res.status(422).send("Password must be atleast 8 characters long")
-        } else if (!isEmail(email)){
-            res.status(422).send("Email must be valid")
+            return res.status(422).send("Password must be minimum 8 charaters long")
+        } else if(!isEmail(email)){
+            return res.status(422).send("Email must be valid")
         }
-        // 2) Check to see if user already exists in DB
+        // 2. Check if user exists in the database
         const user = await User.findOne({email})
         if(user){
             return res.status(422).send(`User already exists with email ${email}`)
         }
-        // 3) --if not, hash their password
+        // 3. --if not hash their password
         const hash = await bcrypt.hash(password, 10)
-        // 4) Create user
+        // 4. Create user
         const newUser = await new User({
             name,
             email,
             password: hash
         }).save()
         console.log({newUser})
-        // 5) Create token for the new user
+        // 5. Create token for the new user
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-            expiresIn: '7d' })
-        // 6) Send back token
+            expiresIn: '7d'
+        })
+        // 6. Send back token in response
         res.status(201).json(token)
     } catch (error) {
         console.error(error)
-        res.status(500).send("Error signing up")
+        res.status(500).send("Error signing up user. Please try again later.")
     }
 }
