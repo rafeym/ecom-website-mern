72f0a89f0555a27173ed2db185319420d34e4fb2
rafeym <mohammadrafey0@gmail.com>
2019-12-04 16:19:09 -0500

    Redirect user upon authentication on server and client

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,21 @@
 import cookie from 'js-cookie'
 import Router from 'next/router'
 
-export function handleLogin (token) {
-    cookie.set('token', token)
-    Router.push('/account')
+export function handleLogin(token) {
+  cookie.set('token', token)
+  Router.push('/account')
+}
+
+// Acceots context object and path to redirect to
+export function redirectUser(ctx, location) {
+  // Redirect on the server
+  if (ctx.req) {
+    // Redirecting
+    ctx.res.writeHead(302, { Location: location })
+    ctx.res.end()
+  }
+  // Redirect on the client
+  else {
+    Router.push(location)
+  }
 }
