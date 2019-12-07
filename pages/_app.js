efe0119f02f85988a42cfe31bf41f5d5e1e2b6ab
rafeym <mohammadrafey0@gmail.com>
2019-12-06 21:56:06 -0500

    Adding user to page props object to make user available within all components

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,52 @@
 import App from 'next/app'
 import Layout from '../components/_App/Layout'
+import { parseCookies } from 'nookies'
+import { redirectUser } from '../utils/auth'
+import baseUrl from '../utils/baseUrl'
+import axios from 'axios'
 
 class MyApp extends App {
   static getInitialProps = async ({ Component, ctx }) => {
+    // Get all our cookies
+    const { token } = parseCookies(ctx)
+
     // Put data from getInitialProps into pageProps
     let pageProps = {}
 
     // Check if a component has a getIntialProp property
-    if(Component.getInitialProps){
+    if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
 
+    // If user does not have a token we dont want to make a unnecessary req
+    if (!token) {
+      const isProtectedRoute =
+        ctx.pathname === '/account' || ctx.pathname === '/create'
+      if (isProtectedRoute) {
+        redirectUser(ctx, '/login')
+      }
+    }
+    // If token exists, try to request user account data
+    else {
+      try {
+        const payload = { headers: { Authorization: token } }
+        const url = `${baseUrl}/api/account`
+        const response = await axios.get(url, payload)
+        const user = response.data
+        pageProps.user = user
+      } catch (error) {
+        console.error('Error getting current user', error)
+      }
+    }
+
     // Return the object that returns the props
-    return {pageProps: pageProps}
+    return { pageProps: pageProps }
   }
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Layout>
-        <Component {...pageProps}/>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
       </Layout>
     )
   }
