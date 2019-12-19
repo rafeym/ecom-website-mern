082466c9d4da61b014cf411afad7134c11ebcf83
rafeym <mohammadrafey0@gmail.com>
2019-12-19 01:15:05 -0500

    FIX: response was missing data field causing token authentication errors

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,6 @@ import { handleLogin } from '../utils/auth'
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react'
 import catchErrors from '../utils/catchErrors'
 
-
 const INITIAL_USER = {
   email: '',
   password: ''
@@ -24,21 +23,20 @@ function Signup() {
     isUser ? setDisabled(false) : setDisabled(true)
   }, [user])
 
-
-  const handleChange = (event) => {
+  const handleChange = event => {
     const { name, value } = event.target
-    setUser(prevState => ({...prevState, [name]: value}))
+    setUser(prevState => ({ ...prevState, [name]: value }))
   }
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async event => {
     event.preventDefault()
     try {
       setLoading(true)
       setError('')
       const url = `${baseUrl}/api/login`
-      const payload = {...user}
-      const response = await axios.post(url,payload)
-      handleLogin(response)
+      const payload = { ...user }
+      const response = await axios.post(url, payload)
+      handleLogin(response.data)
     } catch (error) {
       catchErrors(error, setError)
     } finally {
@@ -46,60 +44,59 @@ function Signup() {
     }
   }
 
-  return <>
-    <Message
-      attached
-      icon="privacy"
-      header="Welcome Back!"
-      content="Login with email and password"
-      color="black"
-    />
-    <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
-    <Message 
-      error
-      header="Oops!"
-      content={error}
-    />
-      <Segment>
-        <Form.Input
-          fluid
-          icon="envelope"
-          iconPosition="left"
-          label="Email"
-          placeholder="Email"
-          name="email"
-          value={user.email}
-          onChange={handleChange}
-          type="email"
-        />
-        <Form.Input
-          fluid
-          icon="lock"
-          iconPosition="left"
-          label="Password"
-          placeholder="Password"
-          name="password"
-          value={user.password}
-          onChange={handleChange}
-          type="password"
-        />
-        <Button
-          disabled={disabled || loading} 
-          icon="sign in"
-          type="submit"
-          color="black"
-          content="Login"
-        />
-      </Segment>
-    </Form>
-    <Message attached="bottom" warning>
-      <Icon name="help"/>
-      New User?{" "}
-      <Link href="/signup">
-        <a>Sign up here</a>
-      </Link>{" "}instead.
-    </Message>
-  </>
+  return (
+    <>
+      <Message
+        attached
+        icon='privacy'
+        header='Welcome Back!'
+        content='Login with email and password'
+        color='black'
+      />
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header='Oops!' content={error} />
+        <Segment>
+          <Form.Input
+            fluid
+            icon='envelope'
+            iconPosition='left'
+            label='Email'
+            placeholder='Email'
+            name='email'
+            value={user.email}
+            onChange={handleChange}
+            type='email'
+          />
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            label='Password'
+            placeholder='Password'
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+            type='password'
+          />
+          <Button
+            disabled={disabled || loading}
+            icon='sign in'
+            type='submit'
+            color='black'
+            content='Login'
+          />
+        </Segment>
+      </Form>
+      <Message attached='bottom' warning>
+        <Icon name='help' />
+        New User?{' '}
+        <Link href='/signup'>
+          <a>Sign up here</a>
+        </Link>{' '}
+        instead.
+      </Message>
+    </>
+  )
 }
 
 export default Signup
