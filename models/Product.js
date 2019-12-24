d3270c829ba602b20643d715e520742a5d65a4a5
rafeym <mohammadrafey0@gmail.com>
2019-12-23 23:38:09 -0500

    REFACTOR: Schema export name changed

--- models/Product.js
+++ models/Product.js
@@ -4,28 +4,29 @@ import shortid from 'shortid'
 const { String, Number } = mongoose.Schema.Types
 
 const ProductSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    sku: {
-        type: String,
-        unique: true,
-        default: shortid.generate
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    mediaUrl: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  sku: {
+    type: String,
+    unique: true,
+    default: shortid.generate
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  mediaUrl: {
+    type: String,
+    required: true
+  }
 })
 
 // If existing Product model exist use that otherwise create new Product schema
-export default mongoose.models.products || mongoose.model('products', ProductSchema)
+export default mongoose.models.products ||
+  mongoose.model('Product', ProductSchema)
