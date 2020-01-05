78b6e61272ea43a8924f0f97fe930a68b3d54a54
rafeym <mohammadrafey0@gmail.com>
2020-01-05 02:13:29 -0500

    DEVELOP: Adding items to cart functionality complete

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,18 +1,77 @@
-import {Input} from 'semantic-ui-react'
+import React from 'react'
+import { Input } from 'semantic-ui-react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import baseUrl from '../../utils/baseUrl'
+import cookie from 'js-cookie'
+import catchErrors from '../../utils/catchErrors'
 
-function AddProductToCart() {
+function AddProductToCart({ user, productId }) {
+  const [quantity, setQuantity] = React.useState(1)
+  const [loading, setLoading] = React.useState(false)
+  const [success, setSuccess] = React.useState(false)
+  const router = useRouter()
+
+  React.useEffect(() => {
+    let timeout
+    if (success) {
+      timeout = setTimeout(() => setSuccess(false), 2000)
+    }
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [success])
+
+  const handleAddProductToCart = async () => {
+    try {
+      setLoading(true)
+      const url = `${baseUrl}/api/cart`
+      const payload = { quantity, productId }
+      // Adding extra layer of security to avoid adding product to cart if user is not authenticated
+      const token = cookie.get('token')
+      const headers = { headers: { Authorization: 'token' } }
+      await axios.put(url, payload, headers)
+      setSuccess(true)
+    } catch (error) {
+      catchErrors(error, window.alert)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Input
-      type="number"
-      min="1"
-      placeholder="Quantity"
-      action={{
-        color: 'orange',
-        content: 'Add to Cart',
-        icon: 'plus cart'
-      }}
+      type='number'
+      min='1'
+      placeholder='Quantity'
+      value={quantity}
+      onChange={event => setQuantity(Number(event.target.value))}
+      action={
+        user && success
+          ? {
+              color: 'blue',
+              content: 'Item Added!',
+              icon: 'plus cart',
+              disabled: true
+            }
+          : user
+          ? {
+              color: 'orange',
+              content: 'Add to Cart',
+              icon: 'plus cart',
+              loading: loading,
+              disabled: loading,
+              onClick: handleAddProductToCart
+            }
+          : {
+              color: 'blue',
+              content: 'Sign up To Purchase',
+              icon: 'signup',
+              onclick: () => router.push('/signup')
+            }
+      }
     />
   )
 }
 
-export default AddProductToCart;
+export default AddProductToCart
