e10b9baf4f91ac082cace5155e628550e7ad75cd
rafeym <mohammadrafey0@gmail.com>
2020-01-05 22:54:36 -0500

    DEVELOP: Cart total is displayed to user of the items within the cart

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,30 @@
+import React from 'react'
+import calculateCartTotal from '../../utils/calculateCartTotal'
 import { Button, Segment, Divider } from 'semantic-ui-react'
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [cartAmount, setCartAmount] = React.useState(0)
+  const [stripeAmount, setStripeAmount] = React.useState(0)
+  const [isCartEmpty, setCartEmpty] = React.useState(false)
+
+  React.useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products)
+    setCartAmount(cartTotal)
+    setStripeAmount(stripeTotal)
+    setCartEmpty(products.length === 0)
+  }, [products])
   return (
     <>
       <Divider />
       <Segment clearing size='large'>
-        <strong>Sub Total:</strong> $0.00
-        <Button icon='cart' color='black' floated='right' content='Checkout' />
+        <strong>Sub Total:</strong> ${cartAmount}
+        <Button
+          icon='cart'
+          disabled={isCartEmpty}
+          color='black'
+          floated='right'
+          content='Checkout'
+        />
       </Segment>
     </>
   )
