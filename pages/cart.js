267072d41034399cb45502b73b23c065f348313a
rafeym <mohammadrafey0@gmail.com>
2020-01-06 20:38:43 -0500

    DEVELOP: Handle checkout and removing item from cart complete

--- pages/cart.js
+++ pages/cart.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Segment } from 'semantic-ui-react'
 
 import CartItemList from '../components/Cart/CartItemList'
@@ -6,12 +7,54 @@ import CartSummary from '../components/Cart/CartSummary'
 import { parseCookies } from 'nookies'
 import axios from 'axios'
 import baseUrl from '../utils/baseUrl'
+import cookie from 'js-cookie'
+import catchErrors from '../utils/catchErrors'
 
 function Cart({ products, user }) {
+  const [cartProducts, setCartProducts] = React.useState(products)
+  const [success, setSuccess] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+
+  const handleRemoveFromCart = async productId => {
+    const url = `${baseUrl}/api/cart`
+    const token = cookie.get('token')
+    const payload = {
+      params: { productId },
+      headers: { Authorization: token }
+    }
+    const response = await axios.delete(url, payload)
+    setCartProducts(response.data)
+  }
+
+  const handleCheckout = async paymentData => {
+    try {
+      setLoading(true)
+      const url = `${baseUrl}/api/checkout`
+      const token = cookie.get('token')
+      const payload = { paymentData }
+      const headers = { headers: { Authorization: token } }
+      const response = await axios.post(url, payload, headers)
+      setSuccess(true)
+    } catch (error) {
+      catchErrors(error, window.alert)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <Segment>
-      <CartItemList products={products} user={user} />
-      <CartSummary products={products} />
+    <Segment loading={loading}>
+      <CartItemList
+        handleRemoveFromCart={handleRemoveFromCart}
+        products={cartProducts}
+        user={user}
+        success={success}
+      />
+      <CartSummary
+        products={cartProducts}
+        handleCheckout={handleCheckout}
+        success={success}
+      />
     </Segment>
   )
 }
