1e93c7fb1d9399ff0786bff51ab41bbd7ef88f3f
rafeym <mohammadrafey0@gmail.com>
2020-01-07 22:29:27 -0500

    DEVELOP: Additon of pages and number of products to be displayed for pagination

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import axios from 'axios'
 import ProductList from '../components/Index/ProductList'
+import ProductPagination from '../components/Index/ProductPagination'
 import baseUrl from '../utils/baseUrl'
 
-function Home({products}) {
-  return <ProductList products={products}/>
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  )
 }
 
 // Fetching data from the server even before the compnent mounts
 // Getting inital data which is being added to the props paramter of component itself
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : '1'
+  const size = 9
   // 1. Fetch data on the server
   const url = `${baseUrl}/api/products`
-  const response = await axios.get(url)
+  const payload = { params: { page, size } }
+
+  const response = await axios.get(url, payload)
   // 2. Return that response data as an object
-  return {products: response.data}
+  return response.data
   // note: this object will be merged with existing props
 }
 
