7a3e513e14061cae0c471172acbfac25f1ec3414
rafeym <mohammadrafey0@gmail.com>
2020-01-07 22:27:22 -0500

    DEVELOP: Products endpoint altered to display certain amounts of prodcuts and support pagination functionality

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,11 +2,28 @@
 import Product from '../../models/Product'
 import connectDb from '../../utils/connectDb'
 
-
 connectDb()
 
-export default async (req,res) => {
-    const products = await Product.find({})
-    res.status(200).json(products)
-    
+export default async (req, res) => {
+  const { page, size } = req.query
+  // 1. Convert query string to numbers
+  const pageNum = Number(page)
+  const pageSize = Number(size)
+
+  let products = []
+
+  const totalDocs = await Product.countDocuments()
+  const totalPages = Math.ceil(totalDocs / pageSize)
+
+  if (pageNum === 1) {
+    products = await Product.find().limit(pageSize)
+  } else {
+    const skips = pageSize * (pageNum - 1)
+    products = await Product.find()
+      .skip(skips)
+      .limit(pageSize)
+  }
+
+  //   const products = await Product.find({})
+  res.status(200).json({ products, totalPages })
 }
