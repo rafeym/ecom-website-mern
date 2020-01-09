a62f192fb08c77fd2d166d2e5523355fea0d9508
rafeym <mohammadrafey0@gmail.com>
2020-01-08 23:10:10 -0500

    DEVELOP: Account info page complete. Displays users information and order history

--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,29 @@
-function Account() {
-  return <>account</>;
+import AccountHeader from '../components/Account/AccountHeader'
+import AccountOrders from '../components/Account/AccountOrders'
+import baseUrl from '../utils/baseUrl'
+
+import { parseCookies } from 'nookies'
+import axios from 'axios'
+
+function Account({ user, orders }) {
+  console.log(orders)
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </>
+  )
 }
 
-export default Account;
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx)
+  if (!token) {
+    return { orders: [] }
+  }
+  const payload = { headers: { Authorization: token } }
+  const url = `${baseUrl}/api/orders`
+  const response = await axios.get(url, payload)
+  return response.data
+}
+
+export default Account
